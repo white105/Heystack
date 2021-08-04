d3892304a105dde9c7f079407cc92564cd3d6ac6
Nick <nickwhite1423@gmail.com>
2021-08-04 18:29:14 -0400

    remove nav header

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import MainApp from "./src/Main";
 import Login from "./src/components/Login";
 import SignUp from "./src/components/SignUp";
 import Home from "./src/components/Home";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -12,8 +12,13 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+    <NavigationContainer theme={{ colors: { background: "#E9FFF9" } }}>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
         <Stack.Screen name="Login">
           {(props) => <Login {...props} />}
         </Stack.Screen>
