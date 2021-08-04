af051edd12ae87a651a6a10193c5b27f3d9ce84f
Fergtic <ferguswhite@outlook.com>
2021-08-04 21:41:18 +0100

    added axios support to sign up and buttons navigation

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -9,6 +9,8 @@ import {
   Pressable,
   TouchableOpacity,
 } from "react-native";
+import axios from "axios"
+
 
 const styles = StyleSheet.create({
   input: {
@@ -17,10 +19,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
   },
-  login: {
+  login:{
     textAlign: "center",
     color: "#1E6738",
-    paddingTop: 10,
+    paddingTop: 10
   },
   signUpScreenButton: {
     marginRight: 40,
@@ -46,55 +48,80 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function SignUp({ navigation }) {
-  const [username, setUsername] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [passwordVerify, setPasswordVerify] = React.useState("");
 
-  return (
-    <View>
-      <SafeAreaView>
-        <Text style={styles.titleText}>
-          {"\n"}
-          {"\n"}
-          {"Sign Up"}
-          {"\n"}
-          {"\n"}
-        </Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          onChangeText={(email) => setEmail(email)}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Username"
-          onChangeText={(username) => setUsername(username)}
-        />
-        <TextInput
-          secureTextEntry
-          style={styles.input}
-          placeholder="Password"
-          onChangeText={(password) => setPassword(password)}
-        />
-        <TextInput
-          secureTextEntry
-          style={styles.input}
-          placeholder="Verify Password"
-          onChangeText={(passwordVerify) => setPasswordVerify(passwordVerify)}
-        />
-      </SafeAreaView>
-      <TouchableOpacity
-        style={styles.signUpScreenButton}
-        onPress={() => console.log(email, username, password, passwordVerify)}
-        underlayColor="#fff"
-      >
-        <Text style={styles.signUpText}>Enter</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-        <Text style={styles.login}>Already have an account? Login here!</Text>
-      </TouchableOpacity>
-    </View>
+
+export default function SignUp({navigation}) {
+  const [username, setUsername] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [passwordVerify, setPasswordVerify] = React.useState('');
+
+
+  const sendSignUpInfo = () => {
+    console.log("email", email);
+    console.log("username", username);
+    console.log("password", password);
+    console.log("passwordVerify", passwordVerify);
+
+    axios({
+      method: "post",
+      url: "http://localhost:3000/signup",
+      data: {
+        email: email,
+        username: username,
+        password: password,
+        passwordVerify: passwordVerify
+      },
+    });
+
+    navigation.navigate("Main");
+  };
+
+    return (
+
+      <View>
+        <SafeAreaView>
+          <Text style={styles.titleText}>
+            {"\n"}
+            {"\n"}
+            {"Sign Up"}
+            {"\n"}
+            {"\n"}
+          </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            onChangeText={(email) =>
+            setEmail(email)}/>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            onChangeText={(username) =>
+            setUsername(username)}/>
+          <TextInput
+            secureTextEntry
+            style={styles.input}
+            placeholder="Password"
+            onChangeText={(password) =>
+            setPassword(password)}/>
+          <TextInput
+            secureTextEntry
+            style={styles.input}
+            placeholder="Verify Password"
+            onChangeText={(passwordVerify) =>
+            setPasswordVerify(passwordVerify)}/>
+        </SafeAreaView>
+        <TouchableOpacity
+          style={styles.signUpScreenButton}
+          onPress={sendSignUpInfo}
+          underlayColor="#fff"
+        >
+          <Text style={styles.signUpText}>Enter</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+          <Text style={styles.login}>Already have an account? Login here!</Text>
+        </TouchableOpacity>
+      </View>
   );
+  
 }
