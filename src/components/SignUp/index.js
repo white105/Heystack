c4da544c969c3aadb5c6f2729d0d2f5dc21558b6
Nick <nickwhite1423@gmail.com>
2021-08-04 17:48:35 -0400

    fix navigation

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -9,8 +9,7 @@ import {
   Pressable,
   TouchableOpacity,
 } from "react-native";
-import axios from "axios"
-
+import axios from "axios";
 
 const styles = StyleSheet.create({
   input: {
@@ -19,10 +18,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
   },
-  login:{
+  login: {
     textAlign: "center",
     color: "#1E6738",
-    paddingTop: 10
+    paddingTop: 10,
   },
   signUpScreenButton: {
     marginRight: 40,
@@ -48,14 +47,11 @@ const styles = StyleSheet.create({
   },
 });
 
-
-
-export default function SignUp({navigation}) {
-  const [username, setUsername] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [passwordVerify, setPasswordVerify] = React.useState('');
-
+export default function SignUp({ navigation }) {
+  const [username, setUsername] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [passwordVerify, setPasswordVerify] = React.useState("");
 
   const sendSignUpInfo = () => {
     console.log("email", email);
@@ -70,58 +66,56 @@ export default function SignUp({navigation}) {
         email: email,
         username: username,
         password: password,
-        passwordVerify: passwordVerify
+        passwordVerify: passwordVerify,
       },
     });
 
-    navigation.navigate("Main");
+    navigation.navigate("Home");
   };
 
-    return (
-
-      <View>
-        <SafeAreaView>
-          <Text style={styles.titleText}>
-            {"\n"}
-            {"\n"}
-            {"Sign Up"}
-            {"\n"}
-            {"\n"}
-          </Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Email"
-            onChangeText={(email) =>
-            setEmail(email)}/>
-          <TextInput
-            style={styles.input}
-            placeholder="Username"
-            onChangeText={(username) =>
-            setUsername(username)}/>
-          <TextInput
-            secureTextEntry
-            style={styles.input}
-            placeholder="Password"
-            onChangeText={(password) =>
-            setPassword(password)}/>
-          <TextInput
-            secureTextEntry
-            style={styles.input}
-            placeholder="Verify Password"
-            onChangeText={(passwordVerify) =>
-            setPasswordVerify(passwordVerify)}/>
-        </SafeAreaView>
-        <TouchableOpacity
-          style={styles.signUpScreenButton}
-          onPress={sendSignUpInfo}
-          underlayColor="#fff"
-        >
-          <Text style={styles.signUpText}>Enter</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-          <Text style={styles.login}>Already have an account? Login here!</Text>
-        </TouchableOpacity>
-      </View>
+  return (
+    <View>
+      <SafeAreaView>
+        <Text style={styles.titleText}>
+          {"\n"}
+          {"\n"}
+          {"Sign Up"}
+          {"\n"}
+          {"\n"}
+        </Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={(email) => setEmail(email)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          onChangeText={(username) => setUsername(username)}
+        />
+        <TextInput
+          secureTextEntry
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={(password) => setPassword(password)}
+        />
+        <TextInput
+          secureTextEntry
+          style={styles.input}
+          placeholder="Verify Password"
+          onChangeText={(passwordVerify) => setPasswordVerify(passwordVerify)}
+        />
+      </SafeAreaView>
+      <TouchableOpacity
+        style={styles.signUpScreenButton}
+        onPress={sendSignUpInfo}
+        underlayColor="#fff"
+      >
+        <Text style={styles.signUpText}>Enter</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+        <Text style={styles.login}>Already have an account? Login here!</Text>
+      </TouchableOpacity>
+    </View>
   );
-  
 }
