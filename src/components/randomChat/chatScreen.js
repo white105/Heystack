957cf217e22b4c05a8c43173405adbd33882dbdb
Fergtic <ferguswhite@outlook.com>
2021-08-04 21:39:48 +0100

    added preliminary screens for

---
+++ src/components/randomChat/chatScreen.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
+
+export default function RoomScreen() {
+  const [messages, setMessages] = useState();
+
+
+  function handleSend(newMessage = []) {
+    setMessages(GiftedChat.append(messages, newMessage));
+  }
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={newMessage => handleSend(newMessage)}
+    />
+  );
+}
